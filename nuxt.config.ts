5c53bbd77ebdcf5079af55d9e0ad8d7d2937dee9
palapalado <ms0529756@gmail.com>
2022-11-25 10:49:27 +0800

    build(runserver setting): runserver setting adjustment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,21 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true
   },
+
   modules: [
-    'nuxt-windicss'
-  ]
+    'nuxt-windicss',
+    '@pinia/nuxt',
+    '@vueuse/nuxt'
+  ],
+
+  css: [
+    'virtual:windi-base.css',
+    'virtual:windi-components.css',
+    'virtual:windi-utilities.css',
+    'animate.css/animate.min.css'
+  ],
+
+  vite: {
+
+  }
 })
