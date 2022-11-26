92c2e36fdabc52d2a5b36c8b065907bbd726fedd
palapalado <ms0529756@gmail.com>
2022-11-26 20:33:54 +0800

    build(tool): font tool

---
+++ scripts/sh/extract_font.sh
@@ -0,0 +1,215 @@
+#/bin/sh
+
+#"0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又"
+
+
+
+echo "/usr/src/app/fonts/EBGaramond/EBGaramond-Bold.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/EBGaramond/EBGaramond-Bold.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-Bold.otf"
+
+echo "/usr/src/app/fonts/EBGaramond/EBGaramond-ExtraBold.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/EBGaramond/EBGaramond-ExtraBold.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-ExtraBold.otf"
+
+echo "/usr/src/app/fonts/EBGaramond/EBGaramond-Medium.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/EBGaramond/EBGaramond-Medium.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-Medium.otf"
+
+echo "/usr/src/app/fonts/EBGaramond/EBGaramond-Regular.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/EBGaramond/EBGaramond-Regular.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-Regular.otf"
+
+echo "/usr/src/app/fonts/EBGaramond/EBGaramond-SemiBold.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/EBGaramond/EBGaramond-SemiBold.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-SemiBold.otf"
+
+echo "/usr/src/app/fonts/ar-julian.woff\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/ar-julian.woff --output-file="/usr/src/app/fonts/small-ar-julian.woff"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Black.otf"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Black.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Black.otf"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Bold.otf"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Bold.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Bold.otf"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Light.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Light.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Light.otf"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Medium.otf"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Medium.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Medium.otf"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Regular.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Regular.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Regular.otf"
+
+echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Thin.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Thin.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Thin.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Black.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Black.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Black.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Medium.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Medium.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Medium.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Regular.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Regular.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Regular.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-SemiBold.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-SemiBold.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-SemiBold.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Bold.otf"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Bold.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Bold.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-ExtraLight.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-ExtraLight.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-ExtraLight.otf"
+
+echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Light.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Light.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Light.otf"
+
+echo "/usr/src/app/fonts/Roboto/Roboto-Regular.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Roboto/Roboto-Regular.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Regular.otf"
+
+echo "/usr/src/app/fonts/Roboto/Roboto-Medium.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Roboto/Roboto-Medium.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Medium.otf"
+
+echo "/usr/src/app/fonts/Roboto/Roboto-Black.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Roboto/Roboto-Black.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Black.otf"
+
+echo "/usr/src/app/fonts/Roboto/Roboto-Bold.otf\n"
+docker run --rm -i -t \
+	-v /home/ec2-user/Taiwan_Tourguide/src/fonts/:/usr/src/app/fonts \
+	pyfonttools:latest \
+	pyftsubset \
+	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載收集顆散落柏蒂豆即獲驚喜唷立競說任歷史記錄擇檔案限或儲存回尚未文件超成功失敗稍試格錯誤確定離取消使用寫期插字輸併修改增優化匯在此書清除建免費移處小綠護永續減碳色生響應環保紙電子省便利又" \
+	--passthrough-tables \
+/usr/src/app/fonts/Roboto/Roboto-Bold.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Bold.otf"
+
+
+sudo chown -R ec2-user:ec2-user ./src/fonts
