c1c36e83f2c3672e1a3fe4234c0bf0ddf53e67a7
palapalado <ms0529756@gmail.com>
2022-11-30 16:38:57 +0800

    style(adjust full height screen style): adjust full height screen style

--- components/MainVisual.vue
+++ components/MainVisual.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="relative flex flex-no-wrap justify-center w-full h-screen 2xl:pb-[3.85417vw]">
+  <div class="relative flex flex-no-wrap justify-center w-full h-screen">
     <LeftCloud class="absolute 2xl:translate-y-[29.375vw] 2xl:translate-x-[-25.20833vw]" />
     <RightCloud class="absolute 2xl:translate-y-[31.59722vw] 2xl:translate-x-[35.17361vw]" />
     <Starting class="absolute self-start 2xl:translate-y-0" />
-    <Playground class="absolute bottom-0 self-end 2xl:bottom-[3.85417vw]" />
+    <Playground class="absolute self-end 2xl:top-[61.73611vw]" />
     <SubjectText class="absolute 2xl:translate-y-[18.61111vw]" />
-    <TrafficLight class="absolute right-0" />
+    <TrafficLight class="absolute right-0 2xl:translate-y-[30.20833vw]" />
     <AttendanceInfo class="absolute 2xl:translate-y-[26.94444vw]" />
     <Dog class="absolute 2xl:translate-y-[34.86111vw] 2xl:translate-x-[-20.69444vw]" />
     <Cat class="absolute 2xl:translate-y-[34.16667vw] 2xl:translate-x-[-0.65972vw]" />
@@ -15,6 +15,6 @@
     />
     <Map class="absolute 2xl:w-[18.05556vw] 2xl:h-[11.80556vw] 2xl:translate-x-[-38.88889vw] 2xl:translate-y-[57.91667vw]" />
     <JoinButton class="absolute  2xl:translate-x-[45.03472vw] 2xl:translate-y-[57.70833vw]" />
-    <SideMenu class="fixed 2xl:left-[-0.34722vw] 2xl:pb-[3.85417vw]" />
+    <SideMenu class="fixed 2xl:left-[-0.34722vw]" />
   </div>
 </template>
--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="h-full flex items-center flex-no-wrap">
+  <div class="h-screen flex flex-no-wrap">
     <img
       class="2xl:w-[1.38889vw] h-full"
       alt="menu"
       src="~/assets/images/bg/bg_menuSide.png"
     >
     <img
-      class="2xl:w-[4.16667vw] 2xl:h-[4.62986vw]"
+      class="2xl:w-[4.16667vw] 2xl:h-[4.62986vw] 2xl:translate-y-[35.20833vw]"
       alt="hide menu"
       src="~/assets/images/btn/btn_burger_open.png"
     >
