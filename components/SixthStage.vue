6ccd0c3a308a4f7538a86ada9067610e06654e3e
palapalado <ms0529756@gmail.com>
2022-12-08 13:55:04 +0800

    feat(add sixth stage 1920 rwd): sixth stage 1920 rwd

--- components/SixthStage.vue
+++ components/SixthStage.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="sixth-stage" class="relative flex justify-center w-full h-screen">
-    <h2 class="sixth-text text-highlight-default tracking-wider transform 2xl:translate-y-[16.11111vw] 2xl:h-[5.20833vw] 2xl:leading-[5.20833vw] ">區區修煉已經無法滿足了嗎？</h2>
+    <h2
+      :class="[
+        'sixth-text text-highlight-default tracking-wider transform flex items-center',
+        '2xl:translate-y-[16.11111vw] 2xl:h-[5.20833vw]',
+        '3xl:translate-y-[12.08333vw] 3xl:h-[3.90625vw]'
+      ]"
+    >
+      區區修煉已經無法滿足了嗎？
+    </h2>
     <img
       alt="sixth-cloud"
       src="~/assets/images/bg/bg_decorate_03.png"
-      class="cloud absolute transform 2xl:translate-y-[27.5vw] 2xl:translate-x-[-22.91667vw] 2xl:w-[34.72222vw] 2xl:h-[12.36111vw]"
+      :class="[
+        'cloud absolute transform ',
+        '2xl:translate-y-[27.5vw] 2xl:translate-x-[-22.91667vw] 2xl:w-[34.72222vw] 2xl:h-[12.36111vw]',
+        '3xl:translate-y-[20.625vw] 2xl:translate-x-[-29.6875vw] 2xl:w-[19.16667vw] 2xl:h-[9.27083vws]'
+      ]"
     >
     <img
       alt="sixth-mumble"
       src="~/assets/images/bg/bg_decorate_07.png"
-      class="mumble absolute 2xl:translate-x-[34.375vw] transform 2xl:translate-y-[20.27778vw] 2xl:w-[31.25vw] 2xl:h-[23.15972vw]"
+      :class="[
+        'mumble absolute transform',
+        '2xl:translate-x-[34.375vw] 2xl:translate-y-[20.27778vw] 2xl:w-[31.25vw] 2xl:h-[23.15972vw]',
+        '3xl:translate-x-[32.34375vw] 3xl:translate-y-[15.20833vw] 3xl:w-[23.4375vw] 3xl:h-[17.36979vw]'
+      ]"
     >
   </div>
 </template>
